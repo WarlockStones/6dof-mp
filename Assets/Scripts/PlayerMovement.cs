170cf06b1c082e3bf324463689430940b1879a7d
W. Stones <warlockstones@protonmail.com>
2024-08-07 11:44:42 +0200

    Add rotations IJKL

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,8 +14,8 @@ public class PlayerMovement : NetworkBehaviour
     Rigidbody rb;
     bool InputsEnabled = false;
 
-    [SerializeField]
-    private float Acceleration = 50;
+    [SerializeField] private float Acceleration = 50;
+    [SerializeField] private float Torque = 20;
 
     PlayerMovement()
     {
@@ -29,18 +29,17 @@ public class PlayerMovement : NetworkBehaviour
 
     void Start()
     {
-        Debug.Log("Start");
         if (IsLocalPlayer)
         {
             InputActions = new IAC_Default();
             InputActions.Gameplay.Enable();
-            InputsEnabled = true;
+            InputsEnabled = true; // TODO: Needed?
         }
 
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    void FixedUpdate()
     {
         if (IsLocalPlayer && InputsEnabled)
         {
@@ -51,18 +50,22 @@ public class PlayerMovement : NetworkBehaviour
              Surge.Value = InputActions.Gameplay.Surge.ReadValue<float>();
              Heave.Value = InputActions.Gameplay.Heave.ReadValue<float>();
              Sway.Value  = InputActions.Gameplay.Sway .ReadValue<float>();
-
             //.. etc
         }
 
         if (IsServer)
         {
             Debug.Log($"Pitch: {Pitch.Value}, Roll: {Roll.Value}, Yaw: {Yaw.Value}, Surge: {Surge.Value}, Heave: {Heave.Value}, Sway: {Sway.Value}");
-            // Clamp inputs and document sus behaviour
+            // Clamp inputs and document sus behaviour. Clamp would only work if not multithreaded. Otherwise you could change it when it is adding force suddenly Surge.Value becomes 300!
+
+            // Rotation
+            rb.AddTorque(transform.forward * Roll.Value  * Torque);
+            rb.AddTorque(transform.right   * Pitch.Value * Torque);
+            rb.AddTorque(transform.up      * Yaw.Value   * Torque);
            
-            rb.AddForce(transform.forward * Surge.Value * Acceleration * Time.deltaTime);
-            rb.AddForce(transform.right   * Sway.Value  * Acceleration * Time.deltaTime);
-            rb.AddForce(transform.up      * Heave.Value * Acceleration * Time.deltaTime);
+            rb.AddForce(transform.forward * Surge.Value * Acceleration);
+            rb.AddForce(transform.right   * Sway.Value  * Acceleration);
+            rb.AddForce(transform.up      * Heave.Value * Acceleration);
         }
     }
 }
