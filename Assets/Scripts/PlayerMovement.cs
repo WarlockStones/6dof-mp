913d3944ace54c69fc393f2c92fef09dac16817e
W. Stones <warlockstones@protonmail.com>
2024-08-08 16:54:16 +0200

    Add bitwise operator test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using Unity.Netcode;
+using Unity.Networking.Transport.Error;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class PlayerMovement : NetworkBehaviour
@@ -18,6 +20,21 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] private float acceleration = 0.01f;
     [SerializeField] private float rotationSpeed = 0.1f;
     [SerializeField] private float maxRotationSpeed = 1.2f;
+
+    private int inputBits; // 16 bits to store the input values
+    const int pitchBits = 0b0000000000000011;
+    const int rollBits  = 0b0000000000001100;
+    const int yawBits   = 0b0000000000110000;
+    const int surgeBits = 0b0000000011000000;
+    const int heaveBits = 0b0000001100000000;
+    const int swayBits  = 0b0000110000000000;
+
+    const int pitchSwitch = 0;
+    const int rollSwitch  = 2;
+    const int yawSwitch   = 4;
+    const int surgeSwitch = 6;
+    const int heaveSwitch = 8;
+    const int swaySwitch  = 10;
 
     PlayerMovement()
     {
@@ -52,6 +69,37 @@ public class PlayerMovement : NetworkBehaviour
             surge.Value = inputActions.Gameplay.Surge.ReadValue<float>();
             heave.Value = inputActions.Gameplay.Heave.ReadValue<float>();
             sway.Value  = inputActions.Gameplay.Sway .ReadValue<float>();
+
+            // IT WORKS!
+            byte s = 0;
+            if (surge.Value > 0)
+                s = 0b01;
+            else if (surge.Value < 0)
+                s = 0b11;
+            inputBits &= ~surgeBits; // Zero out the bits
+            inputBits |= s << surgeSwitch; // Move value s to the correct position
+
+            byte p = 0;
+            if (pitch.Value > 0)
+                p = 0b01;
+            else if (pitch.Value < 0)
+                p = 0b11;
+            inputBits &= ~pitchBits;
+            inputBits |= p << pitchSwitch;
+
+            byte sw = 0;
+            if (sway.Value > 0)
+                sw = 0b01;
+            else if (sway.Value < 0)
+                sw = 0b11;
+            inputBits &= ~swayBits;
+            inputBits |= sw << swaySwitch;
+            Debug.Log("inputBits: "+Convert.ToString(inputBits, 2));
+
+            // Reverse the process
+            // Is what is the sway value? 1 0 -1?
+            int result = (inputBits & surgeBits) >> surgeSwitch;
+            Debug.Log("final surge is: " + Convert.ToString(result, 2));
         }
 
         // TODO: Remove the use of NetworkTransform on client?
